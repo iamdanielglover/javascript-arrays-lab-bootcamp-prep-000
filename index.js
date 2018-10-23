5b322223bc33a682ddfd12ff9c0598af0da8c2d8
Daniel Glover <iamdanielglover@gmail.com>
2018-10-23 13:37:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,9 +31,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  var newArray = kittens;
-  newArray.pop();
-  return newArray;
+  kittens.slice(0,1);
 }
 
 function removeFirstKitten() {
